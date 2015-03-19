123bab7228c999cfa400eab18cfe17f8d172d5fd
kanbara <kanbara@bbtower.co.jp>
2015-03-19 18:06:28 +0900

    add item model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150319084843) do
+ActiveRecord::Schema.define(version: 20150319090343) do
 
   create_table "books", id: false, force: true do |t|
     t.integer  "book_id"
     t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "items", id: false, force: true do |t|
+    t.string   "code"
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
